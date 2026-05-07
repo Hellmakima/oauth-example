219ada8aa4fe1b2f6c650561cb032557fa1caf21
Sufiyan Attar <sufiyanhattar@gmail.com>
2026-05-07 07:47:20 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trying OAuth
+
+Trying out external auth with Authentik and integration with a simple app.
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from trying-oauth!")
+
+
+if __name__ == "__main__":
+    main()
